598b7598e246df86ed75ef1239264ac52850a92e
Tomas Vega <tomasvegagalvez@gmail.com>
2014-08-25 23:04:04 -0700

    removed test added test

--- server.js
+++ server.js
@@ -1,47 +1,23 @@
-var SerialPort  = require('serialport').SerialPort;
-var arduinoPort = '/dev/tty.usbmodem1411';
-
-// setting up the serial connection
-
-var connectArd = function() {
-  var arduinoSerial = new SerialPort(arduinoPort, {
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+// port = '/dev/tty.usbmodem1411';
+port = '/dev/tty.usbmodem1411';
+var serialport = new SerialPort(port, {
     baudRate: 115200,
     dataBits: 8,
     parity: 'none',
     stopBits: 1,
-    flowControl: false
-  });
+    flowControl: false,
+    parser: serialport.parsers.raw    
+});
 
-  arduinoSerial.write('b', function(err, results) {
-  	console.log('err: ' + err);
-  	console.log('results: ' + results);
-  });
-
-  // do something with incoming data
-  arduinoSerial.on('data', function (data) {
-  	console.log(data.toString());
-  });
-
-  arduinoSerial.on('close', function(){
-    console.log('ARDUINO PORT CLOSED');
-    reconnectArd();
-  });
-
-  arduinoSerial.on('error', function (err) {
-    console.error("error", err);
-    reconnectArd();
-  });
-
-}
-
-connectArd();
-
-// check for connection errors or drops and reconnect
-var reconnectArd = function () {
-  console.log('INITIATING RECONNECT');
-  setTimeout(function(){
-    console.log('RECONNECTING TO ARDUINO');
-    connectArd();
-  }, 2000);
-};
-
+serialport.on('open', function(){
+    console.log('Serial Port Opend');
+    serialport.on('data', function(data){
+        console.log(data);
+    });
+    
+    setTimeout(function() {
+        serialport.write('b');
+    }, 5000);
+});
