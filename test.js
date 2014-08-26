44d83b87c0927e9bae13131f5e17ce898a18c9a9
Pierre Karashchuk <krchtchk@gmail.com>
2014-08-25 22:51:09 -0700

    working test

---
+++ test.js
@@ -0,0 +1,23 @@
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+// port = '/dev/tty.usbmodem1411';
+port = '/dev/ttyACM0';
+var serialport = new SerialPort(port, {
+    baudRate: 115200,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false,
+    parser: serialport.parsers.raw    
+});
+
+serialport.on('open', function(){
+    console.log('Serial Port Opend');
+    serialport.on('data', function(data){
+        console.log(data);
+    });
+    
+    setTimeout(function() {
+        serialport.write('b');
+    }, 5000);
+});
